13a3822fa0516e5a6578484a91b590b741a564b8
zkiss97 <zkiss1997@gmail.com>
2022-03-31 10:00:25 +0200

    avada kedavra

--- Vektorok/Program.cs
+++ Vektorok/Program.cs
@@ -6,10 +6,25 @@ using System.Threading.Tasks;
 
 namespace Vektorok
 {
+    class Sorozatok
+    {
+        private int[] numbers = new int[4];
+        private int odds;
+        private float quotient;
+
+        public Sorozatok(int odds, float quotient)
+        {
+            this.odds = odds;
+            this.quotient = quotient;
+        }
+
+        //...
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            // 1. Sorozatok
         }
     }
 }
