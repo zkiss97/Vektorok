b956a4c6bc1d4277e63d590cc5c8dc29ad842178
zkiss97 <zkiss1997@gmail.com>
2022-04-01 10:50:23 +0200

    bruh

--- Vektorok/Program.cs
+++ Vektorok/Program.cs
@@ -8,17 +8,41 @@ namespace Vektorok
 {
     class Sorozatok
     {
-        private int[] numbers = new int[4];
-        private int odds;
-        private float quotient;
+        public int[] numbers = new int[4];
+        public int odds;
+        public float quotient;
 
         public Sorozatok(int odds, float quotient)
         {
             this.odds = odds;
             this.quotient = quotient;
         }
+        public Sorozatok()
+        {
+            this.odds = 0;
+            this.quotient = 1;
+        }
 
-        //...
+        public void Feltolt()
+        {
+            Console.WriteLine("Add meg a számsorozatot:");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        public void Szamtani()
+        {
+            odds = 0;
+            for (int i = 0; i < numbers.Length-1; i++)
+            {
+                odds = numbers[i + 1] - numbers[i];
+                Console.WriteLine(odds);
+            }
+        }
+
     }
 
     class Binaris
@@ -44,20 +68,21 @@ namespace Vektorok
                 if ((number[i] != '0') || (number[i] != '1'))
                 {
                     onlyBin = false;
-                    Console.WriteLine("Érvénytelen");
+                    Console.WriteLine("Érvénytelen. Hibás adat: {0}", i);
                     break;
                 }
             }
 
-            for (int i = 0; i < number.Length; i++)
-            {
-                if (number[i] != '1')
-                {
-                    db++;
-                }
-            }
             if (onlyBin == true)
             {
+                for (int i = 0; i < number.Length; i++)
+                {
+                    if (number[i] != '1')
+                    {
+                        db++;
+                    }
+                }
+
                 Console.WriteLine("A megadott számsorozatban {0} db 1-es számjegy található", db);
             }
         }
@@ -67,14 +92,22 @@ namespace Vektorok
         static void Main(string[] args)
         {
             // 1. Sorozatok
-            // TBD
+            Sorozatok s1 = new Sorozatok();
+            s1.Feltolt();
+            s1.Szamtani();
 
             // 2. Bináris
 
+            /*
             string szam;
+            Binaris b1 = new Binaris();
             Console.WriteLine("Adj meg egy bináris számsort: ");
             szam = Console.ReadLine();
-            Binaris.CountOne(szam);
+            b1.CountOne(szam);
+            */
+
+
+
 
             Console.ReadLine();
         }
